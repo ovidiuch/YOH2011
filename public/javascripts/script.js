cb037f3d5d28d339055d7ce8903341c271341c56
Daniel Mircea <daniel@viseztrance.com>
2011-05-15 03:38:50 +0300

    Reverted to yui 2.9.0

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,99 +1,92 @@
-var counter = 10;
+  var counter = 10;
 
-function countDown() {
-    document.getElementById("countdown").value = counter-- + ' seconds left';
-    if (counter != -1)
-        setTimeout('countDown()',1000);
-    else
-        document.getElementById("countdown").value =' Tough luck.';
-}
+  function countDown() {
+      document.getElementById("countdown").value = counter-- + ' seconds left';
+      if (counter != -1)
+          setTimeout('countDown()',1000);
+      else
+         document.getElementById("countdown").value =' Tough luck.';
+  }
 
-// Define various event handlers for Dialog
-var handleSubmit = function() {
-    this.submit();
-};
-var handleCancel = function() {
-    this.cancel();
-};
-var handleSuccess = function(o) {
-    var response = o.responseText;
-    response = response.split("<!")[0];
-    document.getElementById("resp").innerHTML = response;
-};
-var handleFailure = function(o) {
-    alert("Submission failed: " + o.status);
-};
+  countDown();
 
-YUI().use('event', 'gallery-dialog', function(Y) {
+  function animateValue(container, to_value, timeout) {
+    
+  }
 
-    countDown();
-
-    document.documentElement.className = "yui-pe";
-    // Y.namespace("userdata.container");
-
-    console.log(Y);
-
-    Y.on("domready", function () {
-
-        // Remove progressively enhanced content class, just before creating the module
-        Y.all('#dialog').removeClass('yui-pe-content');
-
-        // Instantiate the Dialog
-        var dialog = new Y.Dialog({
-            width : "30em",
-            fixedcenter : true,
-            visible : false,
-            constraintoviewport : true,
-            buttons : [ { text:"Submit", handler:handleSubmit, isDefault:true },
-                        { text:"Cancel", handler:handleCancel } ]
-        });
-
-        // Validate the entries in the form to require that both first and last name are entered
-        dialog.validate = function() {
-            var data = this.getData();
-            if (data.firstname == "" || data.lastname == "") {
-                alert("Please enter your first and last names.");
-                return false;
-            } else {
-                return true;
-            }
-        };
-
-        // Wire up the success and failure handlers
-        dialog.callback = { success: handleSuccess,
-                            failure: handleFailure };
-
-        // Render the Dialog
-        dialog.render();
-        Y.on("click", dialog.show, "#show");
-        Y.on("click", dialog.hide, "#hide");
-
-        // Y.util.Event.addListener("show", "click", dialog.show, dialog, true);
-        // Y.util.Event.addListener("hide", "click", dialog.hide, dialog, true);
-    });
-});
-
-function animateValue(container, to_value, timeout) {
-
-}
-
-function curveGenerator(start, stop, steps, type) {
+  function curveGenerator(start, stop, steps, type) {
     var i, res = [];
     var lin_incr = (stop - start) / steps;
     var ninty = Math.PI/2; /* ninty degrees */
     var ninty = Math.PI/2; /* ninty degrees */
     for(i = 0; i<=steps; i++) {
-        switch(type) {
+      switch(type) {
         case 'deccel':
-            res.push((stop - start) * Math.sin(ninty * i/steps) + start);
-            break;
+          res.push((stop - start) * Math.sin(ninty * i/steps) + start);
+        break;
         case 'accel':
-            res.push((stop - start) * -Math.sin(ninty * (steps - i)/steps) + stop);
-            break;
+          res.push((stop - start) * -Math.sin(ninty * (steps - i)/steps) + stop);
+        break;
         case 'linear': default:
-            res.push(lin_incr * i + start);
-            break;
-        }
+          res.push(lin_incr * i + start);
+        break;
+      }
     }
     return res;
-}
+  }
+
+  document.documentElement.className = "yui-pe";
+  YAHOO.namespace("userdata.container");
+   
+  YAHOO.util.Event.onDOMReady(function () {
+    
+    // Define various event handlers for Dialog
+    var handleSubmit = function() {
+      this.submit();
+    };
+    var handleCancel = function() {
+      this.cancel();
+    };
+    var handleSuccess = function(o) {
+      var response = o.responseText;
+      response = response.split("<!")[0];
+      document.getElementById("resp").innerHTML = response;
+    };
+    var handleFailure = function(o) {
+      alert("Submission failed: " + o.status);
+    };
+   
+      // Remove progressively enhanced content class, just before creating the module
+      YAHOO.util.Dom.removeClass("dialog", "yui-pe-content");
+   
+    // Instantiate the Dialog
+    YAHOO.userdata.container.dialog = new YAHOO.widget.Dialog("dialog", 
+                { width : "30em",
+                  fixedcenter : true,
+                  visible : false, 
+                  constraintoviewport : true,
+                  buttons : [ { text:"Submit", handler:handleSubmit, isDefault:true },
+                        { text:"Cancel", handler:handleCancel } ]
+                });
+   
+    // Validate the entries in the form to require that both first and last name are entered
+    YAHOO.userdata.container.dialog.validate = function() {
+      var data = this.getData();
+      if (data.firstname == "" || data.lastname == "") {
+        alert("Please enter your first and last names.");
+        return false;
+      } else {
+        return true;
+      }
+    };
+   
+    // Wire up the success and failure handlers
+    YAHOO.userdata.container.dialog.callback = { success: handleSuccess,
+                   failure: handleFailure };
+    
+    // Render the Dialog
+    YAHOO.userdata.container.dialog.render();
+   
+    YAHOO.util.Event.addListener("show", "click", YAHOO.userdata.container.dialog.show, YAHOO.userdata.container.dialog, true);
+    YAHOO.util.Event.addListener("hide", "click", YAHOO.userdata.container.dialog.hide, YAHOO.userdata.container.dialog, true);
+  });
