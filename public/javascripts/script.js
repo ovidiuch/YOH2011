109b6dc3cdfcfeacf411f95c8dfc51e95a68054b
Valentin Caramihai <blankorized@gmail.com>
2011-05-15 10:01:15 +0000

    leave message

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -117,4 +117,17 @@ function curveGenerator(start, stop, steps, type) {
 }
 
 
+function goodbye(e) {
+  if(!e) e = window.event;
+  //e.cancelBubble is supported by IE - this will kill the bubbling process.
+  e.cancelBubble = true;
+  e.returnValue = 'You sure you want to leave?'; //This is displayed on the dialog
+
+  //e.stopPropagation works in Firefox.
+  if (e.stopPropagation) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+}
+window.onbeforeunload=goodbye;
 
